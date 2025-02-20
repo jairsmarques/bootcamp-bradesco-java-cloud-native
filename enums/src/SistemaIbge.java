09688d2892c631c83f2e3993779cddf734aaebb4
Jair Marques <jairsmarques1126@gmail.com>
2025-02-20 09:37:47 -0300

    adicionando o código da aula sobre enums

---
+++ enums/src/SistemaIbge.java
@@ -0,0 +1,19 @@
+
+public class SistemaIbge {
+
+	public static void main(String[] args) {
+		
+		for(EstadoBrasileiro e : EstadoBrasileiro.values()) {
+			System.out.println(e.getSigla() + " - " + e.getNome());
+		}
+		
+		System.out.println("--------------------------");
+		EstadoBrasileiro eb = EstadoBrasileiro.SAO_PAULO;
+		
+		System.out.println(eb.getNome());
+		System.out.println(eb.getSigla());
+		System.out.println(eb.getNomeMaiusculo());
+		System.out.println(eb.getIbge());
+	}
+
+}
