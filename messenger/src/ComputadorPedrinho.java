7ec3e1cc91cbc62cb4e8fc943bd7b85925bf4221
Jair Marques <jairsmarques1126@gmail.com>
2025-02-21 13:06:02 -0300

    adicionando exemplo de herança no código

--- messenger/src/ComputadorPedrinho.java
+++ messenger/src/ComputadorPedrinho.java
@@ -2,12 +2,21 @@
 public class ComputadorPedrinho {
 
 	public static void main(String[] args) {
-		// abrindo MSN Messenger
-		MSNMessenger msn = new MSNMessenger();
 		
-		msn.enviarMensagem();
-		System.out.println("---------------------");
+		System.out.println("MSN");
+		MSNMessenger msn = new MSNMessenger();
+		msn.enviarmensagem();
 		msn.receberMensagem();
+		
+		System.out.println("FACEBOOK");
+		FacebookMessenger fcb = new FacebookMessenger();
+		fcb.enviarmensagem();
+		fcb.receberMensagem();
+		
+		System.out.println("TELEGRAM");
+		Telegram tlg = new Telegram();
+		tlg.enviarmensagem();
+		tlg.receberMensagem();
 	}
 
 }
--- messenger/src/MSNMessenger.java
+++ messenger/src/MSNMessenger.java
@@ -1,23 +1,4 @@
 
-public class MSNMessenger {
+public class MSNMessenger extends ServicoMensagemInstantanea {
 
-	public void enviarMensagem() {
-		validarConectadoInternet();
-		System.out.println("Enviando mensagem");
-		salvarHistoricoMensagem();
-	}
-
-	public void receberMensagem() {
-		validarConectadoInternet();
-		System.out.println("Recebendo mensagem");
-		salvarHistoricoMensagem();
-	}
-
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado á internet");
-	}
-
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-	}
 }
