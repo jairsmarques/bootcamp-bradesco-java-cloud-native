9e96e949487bed2081cded8fdc635aa36c999781
Jair Marques <jairsmarques1126@gmail.com>
2025-02-21 13:40:00 -0300

    trabalhando com polimorfismo

--- messenger/src/ComputadorPedrinho.java
+++ messenger/src/ComputadorPedrinho.java
@@ -1,22 +1,30 @@
+import apps.FacebookMessenger;
+import apps.MSNMessenger;
+import apps.ServicoMensagemInstantanea;
+import apps.Telegram;
 
 public class ComputadorPedrinho {
 
 	public static void main(String[] args) {
+
+		ServicoMensagemInstantanea smi = null;
+
+		/*
+		 * NÃO SE SABE QUAL APP, MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+		 */
+
+		String appEscolhido = "tlg";
+
+		if (appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if (appEscolhido.equals("fbm"))
+			smi = new FacebookMessenger();
+		else if (appEscolhido.equals("tlg"))
+			smi = new Telegram();
 		
-		System.out.println("MSN");
-		MSNMessenger msn = new MSNMessenger();
-		msn.enviarMensagem();
-		msn.receberMensagem();
+		smi.enviarMensagem();
+		smi.receberMensagem();	
 		
-		System.out.println("FACEBOOK");
-		FacebookMessenger fcb = new FacebookMessenger();
-		fcb.enviarMensagem();
-		fcb.receberMensagem();
-		
-		System.out.println("TELEGRAM");
-		Telegram tlg = new Telegram();
-		tlg.enviarMensagem();
-		tlg.receberMensagem();
 	}
 
 }
--- messenger/src/apps/FacebookMessenger.java
+++ messenger/src/apps/FacebookMessenger.java
@@ -1,8 +1,10 @@
+package apps;
 
 public class FacebookMessenger extends ServicoMensagemInstantanea {
 
 	@Override
 	public void enviarMensagem() {
+		validarConectadoInternet();
 		System.out.println("Enviando mensagem pelo Facebook");
 
 	}
