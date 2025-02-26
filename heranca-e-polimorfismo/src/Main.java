34b2023d1861e4c6319fb3537b0563368e552da8
Jair Marques <jairsmarques1126@gmail.com>
2025-02-26 10:15:28 -0300

    adicionando a aula de introdução a herança e polimorfismo

---
+++ heranca-e-polimorfismo/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Manager manager = new Manager();
+		
+		manager.setName("João");
+		//manager.setLogin("joao");
+		//manager.setPassword("123456");
+		
+		System.out.println("Nome: " + manager.getName());
+		//System.out.println("Login: " + manager.getLogin());
+		//System.out.println("Senha: " + manager.getPassword());
+
+	}
+
+}
